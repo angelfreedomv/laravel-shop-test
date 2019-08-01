654b70e50979586c5e6df549c72e8381d4afc139
angelfreedomv <echopan1234@gmail.com>
2019-08-01 11:21:11 +0800

    众筹结束逻辑

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -12,6 +12,7 @@ use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Exceptions\InternalException;
 use App\Models\CrowdfundingProduct;
+use App\Services\OrderService;
 
 class OrdersController extends Controller
 {
@@ -100,7 +101,7 @@ class OrdersController extends Controller
         return redirect()->back();
     }
     
-    public function handleRefund(Order $order, HandleRefundRequest $request)
+    public function handleRefund(Order $order, HandleRefundRequest $request, OrderService $orderService)
     {
         // 判断订单状态是否正确
         if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
@@ -114,8 +115,8 @@ class OrdersController extends Controller
                 $order->update([
                     'extra' => $extra,
                 ]);
-                // 调用退款逻辑
-                $this->_refundOrder($order);
+                // 改为调用封装的方法
+            $orderService->refundOrder($order);
         } else {
             // 将拒绝退款理由放到订单的 extra 字段中
             $extra = $order->extra ?: [];
